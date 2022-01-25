440969cbfe10eb2443ebb481731496bcb04d1329
Haatom <goldom1730@gmail.com>
2022-01-25 18:45:42 -0500

    Imagenes punto 4

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -15,7 +15,7 @@ public class Main {
     
     public static void main(String a[]) throws InterruptedException{
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("200.24.24.55",10000);
+        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55",5000);
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
 
         
