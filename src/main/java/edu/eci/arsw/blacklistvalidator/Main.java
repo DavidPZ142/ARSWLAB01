490cd742e2e4ec5f46a7d74eb58c5862b536d072
DavidPZ666 <davidpz616@gmail.com>
2022-01-25 17:51:28 -0500

    Parte II completa e imagene parte II

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -15,9 +15,9 @@ public class Main {
     
     public static void main(String a[]) throws InterruptedException{
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55",10000);
+        List<Integer> blackListOcurrences=hblv.checkHost("200.24.24.55",10000);
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
-        System.out.println(Runtime.getRuntime().availableProcessors());
+
         
     }
     
